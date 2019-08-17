421753390ba9fb00ca7871207191c8bfd119a5d0
Elton MEdeiros Pereira <eltonmpereira85@outlook.com>
2019-08-17 11:51:29 -0300

    manipulando strings

---
+++ MeuPrimeiroTerminal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MeuPrimeiroTerminal
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            string txt2 = "Elton, com idade de: ";
+            string txt1 ="Hello World";
+            int idade = 33;
+            string txt3 = $"{txt1} {txt2} {idade}";
+            string idadeEmString = idade.ToString();
+
+            Console.WriteLine(txt3);
+        }
+    }
+
+}
