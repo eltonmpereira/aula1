8eb55c84ed5929bb895df11f700819fc32385860
Elton MEdeiros Pereira <eltonmpereira85@outlook.com>
2019-08-20 14:20:14 -0300

    imcComIf

--- MeuPrimeiroTerminal/Program.cs
+++ MeuPrimeiroTerminal/Program.cs
@@ -4,41 +4,43 @@ namespace MeuPrimeiroTerminal
 {
     class Program
     {
-               static void Main(string[] args)
-        {
-            Console.WriteLine ("Qual time ganhou a ultima Copa?");
-            string[] times = new string[4];
-            times [0] = "Brasil";
-            times [1] = "Franca";
-            times [2] = "México";
-            times [3] = "Alemanha";
-            for(int i = 0; i<4;i++)
-            {
-                Console.WriteLine (times[i]);
-            }
-
-            Console.WriteLine ("Resposta: ");
+        static void Main(string[] args)
+        {   float imc, peso, altura;
+            Console.WriteLine("Digite o Seu peso: ");
+            peso = float.Parse (Console.ReadLine());
             
-            String opcaoDoUsuario = Console.ReadLine();
-            bool ehVencedor = EhVencedor(opcaoDoUsuario);
+            
+            Console.WriteLine("Digite sua altura: ");
+            altura = float.Parse (Console.ReadLine());
 
-            if(ehVencedor)
-                Console.WriteLine ("Venceu");
-            else
-                Console.WriteLine ("Perdeu");            
-        }
+            imc = (peso /(altura * altura));
 
-        public static bool EhVencedor(string opcaoDoUsuario)
-        {
-            string vencedor = "Franca";
-            // if(opcaoDoUsuario == vencedor)
-            //{
-            //    return true;
-            //}
+            Console.WriteLine("Seu IMC é:" + imc);
 
-            //return false;
-
-            return opcaoDoUsuario == vencedor;
+            if (imc<18.5)
+            {
+                Console.Write ("Peso Abaixo do Normal");
+            }
+            else if (imc >18.5 && imc < 25)
+            {
+                Console.WriteLine ("Peso Normal");
+            }
+            else if (imc > 25 && imc <30)
+            {
+                Console.WriteLine ("Sobrepeso.");
+            }
+            else if (imc >30 && imc <35)
+            {
+                Console.WriteLine ("Obesidade Grau 1");
+            }
+            else if (imc>35 && imc<40)
+            {
+                Console.WriteLine("Obesidade Grau 2");
+            }
+            else if (imc>40)
+            {
+                Console.WriteLine("Obesidade Grau 3");
+            }
         }
     }
 
